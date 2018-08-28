4943f6cee5df57ce0a724a1072f9afd36096a72c
Jens Eliasson <jens.eliasson@gmail.com>
2018-08-28 08:24:59 +0200

    switched from string to proper JSON object generation in responses

--- src/main/java/eu/thingwave/datamanager/resources/StatusHTTPResource.java
+++ src/main/java/eu/thingwave/datamanager/resources/StatusHTTPResource.java
@@ -33,9 +33,11 @@ public class StatusHTTPResource extends HttpServlet
   Properties prop = null;
   boolean srok = false, drok = false, orchok=false;
 
+
   public StatusHTTPResource(Properties prop){
     this.prop = prop;
   }
+
 
   public void setStatus(String name, boolean status){
 
@@ -56,23 +58,42 @@ public class StatusHTTPResource extends HttpServlet
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
   {
-    String res = "[\n";
-    String linebegin = "", lineend="";
-
-    res += "  {\"SysName\": \""+prop.getProperty("SysName", "")+"\"},\n";
+    //String res = "[\n";
+    //res += "  {\"SysName\": \""+prop.getProperty("SysName", "")+"\"},\n";
+    //String linebegin = "", lineend="";
+    JSONArray reslist = new JSONArray();
 
     if (!prop.getProperty("ahf-sr", "").equals("")) {
-      res += "  {\"name\": \"_ahf._sr.tcp.xxx\", \"type\": \"ServiceRegistry\", \"URI\": \""+prop.getProperty("ahf-sr", "")+"\", \"status\": "+srok+"}"; linebegin = ",\n";
+      //res += "  {\"name\": \"_ahf._sr.tcp.xxx\", \"type\": \"ServiceRegistry\", \"URI\": \""+prop.getProperty("ahf-sr", "")+"\", \"status\": "+srok+"}"; linebegin = ",\n";
+      JSONObject obj = new JSONObject();
+      obj.put("name", "_ahf._sr.tcp.xxx");
+      obj.put("type", "ServiceRegistry");
+      obj.put("URI", prop.getProperty("ahf-sr", ""));
+      obj.put("status", srok);
+      reslist.add(obj);
     }
     if (!prop.getProperty("ahf-dr", "").equals("")) {
-      res += linebegin+ "  {\"name\": \"_ahf._dr.tcp.xxx\", \"type\": \"DeviceRegistry\", \"URI\": \""+prop.getProperty("ahf-dr", "")+"\", \"status\": "+drok+"}"; linebegin = ",\n";
+      //res += linebegin+ "  {\"name\": \"_ahf._dr.tcp.xxx\", \"type\": \"DeviceRegistry\", \"URI\": \""+prop.getProperty("ahf-dr", "")+"\", \"status\": "+drok+"}"; linebegin = ",\n";
+      JSONObject obj = new JSONObject();
+      obj.put("name", "_ahf._dr.tcp.xxx");
+      obj.put("type", "DeviceRegistry");
+      obj.put("URI", prop.getProperty("ahf-dr", ""));
+      obj.put("status", drok);
+      reslist.add(obj);
     }
     if (!prop.getProperty("ahf-orch", "").equals("")) {
-      res +=  linebegin + "  {\"name\": \"_ahf._orch.tcp.xxx\", \"type\": \"Orchestration\", \"URI\": \""+prop.getProperty("ahf-orch", "")+"\", \"status\": "+orchok+"}\n";
+      //res +=  linebegin + "  {\"name\": \"_ahf._orch.tcp.xxx\", \"type\": \"Orchestration\", \"URI\": \""+prop.getProperty("ahf-orch", "")+"\", \"status\": "+orchok+"}\n";
+      JSONObject obj = new JSONObject();
+      obj.put("name", "_ahf._orch.tcp.xxx");
+      obj.put("type", "Orchestration");
+      obj.put("URI", prop.getProperty("ahf-orch", ""));
+      obj.put("status", orchok);
+      reslist.add(obj);
     } 
 
-    res += "\n]";
-    response.getWriter().println(res);
+    //res += "\n]";
+    //response.getWriter().println(res);
+    response.getWriter().println(reslist);
 
     return;
   }
